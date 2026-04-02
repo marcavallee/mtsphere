847737d45b050f9c649fcc4eb4c29ce0e9a9f2a6
marcavallee <mvallee@geodatasolutions.ca>
2026-04-02 12:57:34 -0400

    Add files via upload

--- python/hooks/hook-pyshtools.py
+++ python/hooks/hook-pyshtools.py
@@ -1,1 +1,11 @@
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+import os
 
+datas = collect_data_files('pyshtools')
+hiddenimports = collect_submodules('pyshtools')
+
+# Brute force the entire doc directory
+pyshtools_path = os.path.dirname(__import__('pyshtools').__file__)
+doc_path = os.path.join(pyshtools_path, 'doc')
+if os.path.exists(doc_path):
+    datas.append((doc_path, 'pyshtools/doc'))
